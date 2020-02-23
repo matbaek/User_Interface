b5f884c73495615f85ef2887bbc9c2151a13093c
Mathias Bærentsen <mathias.barentsen@gmail.com>
2020-02-23 17:41:17 +0100

    Sende data fra Activity til xml.

--- app/src/main/java/dk/offlines/userinterface/MainActivity.kt
+++ app/src/main/java/dk/offlines/userinterface/MainActivity.kt
@@ -1,41 +1,34 @@
 package dk.offlines.userinterface
 
+import android.graphics.Color
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var container: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "You entered the text of ${editText.text}", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        container = findViewById(R.id.linearLayout)
+
+        addTextView("Hello")
+        addTextView("From")
+        addTextView("Android")
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+    private fun addTextView(label: String) {
+        val view = TextView(this)
+        view.text = label
+        view.textSize = 28f
+        view.setTextColor(Color.parseColor("#FF0000"))
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+        container.addView(view)
     }
 }
